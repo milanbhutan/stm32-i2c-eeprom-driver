e4099d3d5300245c65f16278f6c2a2349773cf40
milanbhutan <milanbhutan@gmail.com>
2026-04-29 12:40:35 -0700

    testing

--- Core/Src/EEPROM.c
+++ Core/Src/EEPROM.c
@@ -65,7 +65,7 @@ void EEPROM_write_string(char str[],char addr[]){
 	// wait for start condition to transmit
 	while(!(I2C1->ISR & I2C_ISR_TXIS)) ;
 	// xmit MSByte of address
-	I2C1->TXDR = (mem_addr >> 8);
+	I2C1->TXDR = (mem_addr >> 8); //iosd
 
 	while(!(I2C1->ISR & I2C_ISR_TXIS)) ;
 	// xmit LSByte of address
